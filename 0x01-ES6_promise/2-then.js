392c1ecbbe7628f00140167ff23e22caddb77c63
Chiemezie Jacob <chiemezie.jacob@gmail.com>
2023-06-27 15:17:36 +0100

    2-then.js

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,11 +1,5 @@
 export default function handleResponseFromAPI(promise) {
-  return new Promise((resolve, reject) => {
-    if (promise) {
-      console.log('Got a response from the API');
-      resolve({
-        status: 200,
-        body: 'Success',
-      });
-    } reject(new Error());
-  });
+  promise.then(() => ({ status: 200, body: 'Success' }))
+    .catch(() => new Error())
+    .finally(console.log('Got a response from the API'));
 }
