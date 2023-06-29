f72988aa5133f41b8f91e1c9c48cf5612ae0cef5
Chiemezie Jacob <chiemezie.jacob@gmail.com>
2023-06-29 23:36:56 +0100

    Declared a class and used the getter and setter function

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,42 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    this._name = name; // eslint-disable-line
+    this._length = length; // eslint-disable-line
+    this._students = students; // eslint-disable-line
+  }
+
+  get name() {
+    return this._name; // eslint-disable-line
+  }
+
+  set name(name) {
+    if (typeof name !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    this._name = name; // eslint-disable-line
+  }
+
+  get length() {
+    return this._length; // eslint-disable-line
+  }
+
+  set length(length) {
+    if (typeof length !== 'number') {
+      throw new TypeError('Length must be a number');
+    }
+    this._length = length; // eslint-disable-line
+  }
+
+  get students() {
+    return this._students; // eslint-disable-line
+  }
+
+  set students(value) {
+    value.forEach((element) => {
+      if (typeof element !== 'string') {
+        throw new TypeError('Students must be an array of strings');
+      }
+    });
+    this._students = value; // eslint-disable-line
+  }
+}
