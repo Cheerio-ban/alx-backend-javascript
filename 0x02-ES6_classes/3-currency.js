43f83c77228929753c15348d07690b194b3e84ef
Chiemezie Jacob <chiemezie.jacob@gmail.com>
2023-06-29 23:55:56 +0100

    Class with getter, setter and method

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,32 @@
+export default class Currency {
+  constructor(code, name) {
+    this._code = code; // eslint-disable-line
+    this._name = name; // eslint-disable-line
+  }
+
+  get code() {
+    return this._code; // eslint-disable-line
+  }
+
+  set code(value) {
+    if (typeof value !==  'string') {
+      throw new TypeError('Code must be a string');
+    }
+    this._code = value; // eslint-disable-line
+  }
+
+  get name() {
+    return this._name; // eslint-disable-line
+  }
+
+  set name(value) {
+    if (typeof value !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    this._name = value; // eslint-disable-line
+  }
+
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`; // eslint-disable-line
+  }
+}
