708162f1809f88f5c8eb0d0fdf656ff1f2f9e879
Chiemezie Jacob <chiemezie.jacob@gmail.com>
2023-07-03 22:05:29 +0100

    Working with Map

---
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -0,0 +1,10 @@
+export default function updateUniqueItems(aMap) {
+  if (!(aMap instanceof Map)) {
+    throw new Error('Cannot process');
+  }
+  for (const [key, value] of aMap) {
+    if (value === 1) {
+        aMap.set(key, 100);
+    }
+  }
+}
