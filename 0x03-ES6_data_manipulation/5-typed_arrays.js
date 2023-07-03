d487b8f66f56fb3c6216b89c390bebbf4bea298a
Chiemezie Jacob <chiemezie.jacob@gmail.com>
2023-07-03 19:44:39 +0100

    Array buffer

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -2,7 +2,7 @@ export default function createInt8TypedArray(length, position, value) {
   const buffer = new ArrayBuffer(length);
   const int8view = new Int8Array(buffer);
 
-  if (position > buffer.byteLength / 4) {
+  if (position > buffer.byteLength) {
     throw new Error('Position outside range');
   }
   int8view[position] = value;
