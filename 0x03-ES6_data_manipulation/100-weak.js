04bd63875085657bc9d138d8c6819ef119c3c1bf
Chiemezie Jacob <Chiemezie.jacob@gmail.com>
2023-07-05 02:08:00 +0100

    Weak JS

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,6 +1,6 @@
 export const weakMap = new WeakMap();
 
-export default function queryAPI(endpoint) {
+export const queryAPI = (endpoint) => {
  if (weakMap.has(endpoint)) {
     weakMap.set(endpoint, weakMap.get(endpoint) + 1);
  } else {
@@ -9,4 +9,4 @@ export default function queryAPI(endpoint) {
  if (weakMap.get(endpoint) >= 5) {
     throw new Error('Endpoint load is high')
  }
-}
+};
