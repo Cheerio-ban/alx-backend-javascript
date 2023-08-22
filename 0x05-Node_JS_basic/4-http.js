dab283bfbd8b3f61151e34177235c4ce2d774128
Chiemezie Jacob <chiemezie.jacob@gmail.com>
2023-08-22 14:59:44 +0100

    Http server

---
+++ 0x05-Node_JS_basic/4-http.js
@@ -0,0 +1,16 @@
+const http = require('http');
+
+const port = 1245;
+const host = 'localhost';
+
+const app = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello Holberton School!');
+});
+
+app.listen(port, host, () => {
+  console.log(`Server running at http://${host}:${port}/`);
+});
+
+module.exports = app;
