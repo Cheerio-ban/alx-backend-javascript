e289715a3ffc07484b5816177df919d01484c41c
Chiemezie Jacob <chiemezie.jacob@gmail.com>
2023-08-22 05:43:42 +0100

    Fixed newline error

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,7 +1,8 @@
 console.log('Welcome to Holberton School, what is your name?');
 process.stdin.on('data', (data) => {
   if (data) {
-    console.log(`Your name is: ${data.toString().trim()}`);
+    const template = `Your name is: ${data}`;
+    process.stdout.write(template);
     process.exit();
   }
 });
