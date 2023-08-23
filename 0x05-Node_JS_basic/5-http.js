cca43a55b0a0f8e14c5e4f0ecdb769da63d21ffa
Chiemezie Jacob <chiemezie.jacob@gmail.com>
2023-08-23 11:02:33 +0100

    Http server

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,4 +1,53 @@
 const http = require('http');
+const { readFile } = require('fs');
+
+const printStudents = (dataa) => {
+  const data = dataa.split('\n');
+  const newarr = [];
+  for (const elem of data) {
+    newarr.push(elem.split(','));
+  }
+
+  const newhash = {};
+  let output = '';
+  for (const elem of newarr) {
+    const len = elem.length - 1;
+    if (elem[len] in newhash) {
+      newhash[elem[len]].push(elem[0]);
+    } else {
+      newhash[elem[len]] = [elem[0]];
+    }
+  }
+  let students = 0;
+  for (const [k, v] of Object.entries(newhash)) {
+    if (k !== 'field') {
+      if (k.trim() !== '') {
+        students += v.length;
+      }
+    }
+  }
+  output += `Number of students: ${students}\n`;
+  for (const [k, v] of Object.entries(newhash)) {
+    if (k !== 'field') {
+      if (k.trim() !== '') {
+        let arrstr = v.toString();
+        arrstr = arrstr.split(',');
+        output += `Number of students in ${k}: ${v.length}. List: ${arrstr.join(', ')}\n`;
+      }
+    }
+  }
+  return output;
+};
+
+const countStudents = (filename) => new Promise((resolve, reject) => {
+  readFile(filename, 'utf8', (err, data) => {
+    if (err) {
+      reject(err);
+    } else {
+      resolve(printStudents(data));
+    }
+  });
+});
 
 const port = 1245;
 const host = 'localhost';
@@ -6,12 +55,19 @@ const host = 'localhost';
 const app = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  url = req.url;
-  
+  const { url } = req;
+
   if (url === '/') {
     res.end('Hello Holberton School!');
   } else if (url === '/students') {
-    res.end('This is the list of our students');
+    res.write('This is the list of our students\n');
+    countStudents(process.argv[2]).then((data) => {
+      const newString = String(data);
+      res.end(newString);
+    }).catch(() => {
+      res.statusCode = 404;
+      res.end('Cannot load the database');
+    });
   }
 });
 
