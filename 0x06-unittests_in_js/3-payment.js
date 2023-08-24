0d7eebd26f53cca0bb13619e4531d5646a3f3e5d
Chiemezie Jacob <chiemezie.jacob@gmail.com>
2023-08-24 12:17:04 +0100

    Sinon

---
+++ 0x06-unittests_in_js/3-payment.js
@@ -0,0 +1,8 @@
+const utils = require('./utils');
+
+const sendPaymentRequestToApi = (totalAmount, totalShipping) => {
+  const added = utils.calculateNumber('SUM', totalAmount, totalShipping);
+  console.log(`The total is: <result of the sum>The total is: ${added}`);
+};
+
+module.exports = sendPaymentRequestToApi;
